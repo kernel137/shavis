f575148520fe8fe638c14667e0b7fd520c096db8
Kernel <kostamecev@pm.me>
2024-02-27 01:00:05 +0100

    Added theme loader and the first theme

--- sha256vis.py
+++ sha256vis.py
@@ -1,34 +1,49 @@
 from PIL import Image
+
 sha256 = str(input("sha256sum: "))
-
-
-# [===================================================]
+# [==================================================]
+# Check length
 if len(sha256) != 64: 
 	print("Error: Input must be 64 characters long.")
 	exit()
 
+# Check for invalid characters (And point them out!!!)
 hex_allowed = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "a", "b", "c", "d", "e", "f"]
 valid_characters = [i in hex_allowed for i in sha256]
 if not all(valid_characters): 
 	print("Error: Invalid SHA256 hashsum character in string.")
+	print(sha256)
 	for i in range(len(valid_characters)):
-		if not valid_characters[i]: print(f"index: {i}\ncharacter: {sha256[i]}")
+		if not valid_characters[i]: print("↑", end='')
+		else: print(" ", end='')
+	print()
 	exit()
-
+# [==================[ Processing ]==================]
 sha256_dv = [int(i, 16) for i in sha256] # dv  = Decimal Values
 sha256_dvm = []		 					 # dvm = Decimal Values Matrix
 for i in range(0, 8): sha256_dvm.append(sha256_dv[i*8:(i+1)*8])
 
-# [===================================================]
+# [==================[Theme Loader]==================]
+theme = []
+with open("./themes/dark.hex") as file:
+	theme = file.readlines()
+for i in range(16): theme[i] = theme[i].strip()
+for i in range(16): theme[i] = tuple(int(theme[i][i2:i2+2], 16) for i2 in [0, 2, 4])
+# [==================================================]
 
 
-for i in range(8):
-	for i2 in range(8):
-		print(((sha256_dvm[i][i2]+1)*16)-1, end=' ')
-	print()
+
+# for i in range(8):
+# 	for i2 in range(8):
+# 		print(((sha256_dvm[i][i2]+1)*16)-1, end=' ')
+# 	print()
 
 # [Implement size here]
 
+size_select = 7
+# 1 2  3  4  5   6   7   8    9    10
+size = 8 * (2**(size_select-1))
+# 8 16 32 64 128 256 512 1024 2048 4096 
 
 # [===================]
 
@@ -43,7 +58,7 @@ for v in range(8):
 		pixels[v,h] = ((sha256_dvm[h][v]+1)*16)-1
 		
 
-image.resize((512,512), resample=Image.NEAREST).show()
+image.resize((size,size), resample=Image.NEAREST).show()
 
 
 
